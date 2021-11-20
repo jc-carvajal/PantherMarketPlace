4c3e3f2aba6e3e0eb1e07519f578ef80ce34d301
CHRONOS-PC\Chronos <chrono80@hotmail.com>
2021-11-19 20:58:23 -0500

    -La aplicación ahora inicia desde la vista de inicio de sesion (modificado el launch en el manifest) -Cambiado a fondo con brillo nivel 0 -Se ha agregado opciónes de registro desde el inicio (presionando el botón manda a la vista de registro) -El botón de iniciar sesión manda directamente a la vista de Activity Main (Aun no pide cuentas para poder entrar) -Recursos adicionales en fuentes (strings.xml)

--- app/src/main/java/com/pchronos/septimaappjava/Login.java
+++ app/src/main/java/com/pchronos/septimaappjava/Login.java
@@ -1,13 +1,31 @@
 package com.pchronos.septimaappjava;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button Bini,Breg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Bini=(Button) findViewById(R.id.inicio_boton_log_in);
+    }
+
+    public void iniciarSesion(View view) {
+
+            Intent SA=new Intent(view.getContext(),MainActivity.class);
+            startActivity(SA);
+
+    }
+
+    public void crearUsuario(View view) {
+        Intent AFR=new Intent(this,FormularioRegistro.class);
+        startActivity(AFR);
     }
 }
